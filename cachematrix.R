5e17ed4d45540bd659c3f47161bca19c7cbff909
Timber Lee <ntutblee@gmail.com>
2015-03-19 00:20:38 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions makeCacheMatrix and cacheSolve are defined as below
+## The makeCacheMatrix defines an matrix type object with 4 different operations: set, get, setinverse and getinverse
 
-## Write a short comment describing this function
+## see line by line comments
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Two values are stored in the object (1) matrix value x (2) inverse matrix value mInverse
+  mInverse<-NULL ## initialize the object, set inverse as NULL
+  set<-function(mInput){
+    x<<-mInput ## assign the matrix value to the defining environment
+    mInverse<<-NULL ## initialize the object, set inverse as NULL
+  }
+  get<-function(){
+    x ## return object value
+  }
+  setinverse<-function(mInputInverse){
+    mInverse<<-mInputInverse ## assign the inverse matrix to the defining invironment
+  }
+  getinverse<-function(){
+    mInverse ## return inverse matrix value
+  }
+  list(
+    set = set,
+    get = get,
+    setinverse = setinverse,
+    getinverse = getinverse ## the return value of function mkeCacheMatrix is a list of 4 functions
+    )
 }
 
 
-## Write a short comment describing this function
+## see line by line comments
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mSolution<-x$getinverse() ## get inverse value from the object
+  if( !is.null(mSolution) ){ ## check if the inverse has been calculated before
+    message("getting cached data") ## if yes, display message
+    return(mSolution) ## inverse has been calculated before, return the cached value
+  }
+  ## when the inverse has not been calculated before
+  data<-x$get() ## obtain the matrix value of the object
+  mSolution<-solve(data,...) ## calculate inverse, use ... operator to pass additional setting for inverse calculation
+  x$setinverse(mSolution) ## set and save the inverse matrix value into the object
+  mSolution ## return the inverse matrix value
 }
